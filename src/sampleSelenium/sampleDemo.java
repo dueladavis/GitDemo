56c2a5ee232e4903622bb6f86016136a8b3e7175
dueladavis <dueladavis123@gmail.com>
2020-09-30 18:36:01 +0530

    This is commited from US

--- src/sampleSelenium/sampleDemo.java
+++ src/sampleSelenium/sampleDemo.java
@@ -10,6 +10,7 @@ public class sampleDemo {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Galaxy 39\\Documents\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
+		System.out.println("Successfully updated to GitHub");
 	}
 
 }

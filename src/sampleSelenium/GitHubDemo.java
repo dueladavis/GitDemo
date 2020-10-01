766bb9278e6fe4b3579f032a6084e90a5bfed9fc
dueladavis <dueladavis123@gmail.com>
2020-10-01 10:35:28 +0530

    Asian Development

--- src/sampleSelenium/GitHubDemo.java
+++ src/sampleSelenium/GitHubDemo.java
@@ -3,15 +3,16 @@ package sampleSelenium;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class sampleDemo {
+public class GitHubDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Galaxy 39\\Documents\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
-		System.out.println("Successfully updated to GitHub");
-		System.out.println("Successfully created the develop branch");
+		System.out.println("Successfully initiated the Project");
+		System.out.println("Successfully commited and push the Project");
+	
 	}
 
 }
